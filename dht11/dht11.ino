73bbeceb956f2fa1a366fe0ce830e582d7dad8d0
TVBinK <bink060603@gmail.com>
2024-09-20 00:14:27 +0700

    connect blynk

--- dht11/dht11.ino
+++ dht11/dht11.ino
@@ -1,32 +1,60 @@
+#define BLYNK_TEMPLATE_ID "TMPL6ZUB3eOGq"
+#define BLYNK_TEMPLATE_NAME "Smart Home"
+#define BLYNK_AUTH_TOKEN "5tH-zqq3caTuYxR_Fq675QFDaIY63V1A"
+
 #include "DHT.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#define DPIN 4        //Pin to connect DHT sensor (GPIO number)
-#define DTYPE DHT11   // Define DHT 11 or DHT22 sensor type
+#include <WiFi.h>                // Thư viện WiFi cho ESP32
+#include <BlynkSimpleEsp32.h>    // Thư viện Blynk cho ESP32
 
-DHT dht(DPIN,DTYPE);
+#define DPIN 4        // Pin nối cảm biến DHT (GPIO số)
+#define DTYPE DHT11   // Định nghĩa loại cảm biến DHT11 hoặc DHT22
+
+DHT dht(DPIN, DTYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-void setup() {
-  lcd.init();                       // Initialize the LCD
-  lcd.backlight();                  // Turn on the backlight
-  lcd.clear();                      // Clear the LCD screen
 
+// Kết nối WiFi và Blynk
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Phong507";  // Thay bằng tên WiFi của bạn
+char pass[] = "23456789";  // Thay bằng mật khẩu WiFi của bạn
+
+void setup() {
+  // Khởi tạo LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  // Khởi tạo cổng Serial
   Serial.begin(9600);
+  
+  // Khởi động Blynk
+  Blynk.begin(auth, ssid, pass);
+  
+  // Khởi động cảm biến DHT
   dht.begin();
 }
 
 void loop() {
-  lcd.setCursor(0, 0); 
-  float tc = dht.readTemperature(false);  //Read temperature in C
-  float hu = dht.readHumidity();          //Read Humidity
-
+  Blynk.run();  // Điều khiển kết nối với Blynk
+  
+  // Đọc nhiệt độ và độ ẩm
+  double tc = dht.readTemperature(false);  // Đọc nhiệt độ (C)
+  double hu = dht.readHumidity();          // Đọc độ ẩm
+  
+  // Hiển thị dữ liệu lên LCD
+  lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(tc);
   lcd.print(" C ");
-  lcd.setCursor(0, 1); 
+  lcd.setCursor(0, 1);
   lcd.print("Hum: ");
   lcd.print(hu);
-  lcd.println(" %                 ");
+  lcd.print(" %                 ");
+  
+  // Gửi dữ liệu lên Blynk
+  Blynk.virtualWrite(V0, tc);  // Gửi nhiệt độ lên Virtual Pin V0
+  Blynk.virtualWrite(V1, hu);  // Gửi độ ẩm lên Virtual Pin V1
 
-  delay(2000);
+  delay(2000);  // Chờ 2 giây trước khi lặp lại
 }
